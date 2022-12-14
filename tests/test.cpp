a898474c6f4f39b56be95cf2d9ace70da48d7882
endropia <liza.b180802@gmail.com>
2022-12-14 21:17:29 +1000

    check args in test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,13 +1,17 @@
 #include "tester.h"
+#include "../args.h"
 #include <iostream>
 
 
-int main() {
-    auto res = LexerTester("../tests/lexer").RunTests();
-    res += ParserTester("../tests/parser").RunTests();
-
+int main(int argc, char **argv) {
+    TestResult res;
+    if (check_arg(argc, argv, "-l")) {
+        res += LexerTester("../tests/lexer").RunTests();
+    }
+    if (check_arg(argc, argv, "-p")) {
+        res += ParserTester("../tests/parser").RunTests();
+    }
     std::cout << res;
-
     return 0;
 }
 
