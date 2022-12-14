5970a9cd5361a0658d649f0e1feba4ba02e2aa71
endropia <liza.b180802@gmail.com>
2022-12-14 21:15:56 +1000

    move function for check arguments to args.(h|cpp)

---
+++ args.cpp
@@ -0,0 +1,11 @@
+#include "args.h"
+#include <string.h>
+
+bool check_arg(int argc, char **argv, const std::string &arg) {
+    for (auto i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], arg.c_str()) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ args.h
@@ -0,0 +1,8 @@
+#ifndef COMPILER_ARGS_H
+#define COMPILER_ARGS_H
+
+#include <string>
+
+bool check_arg(int argc, char **argv, const std::string &arg);
+
+#endif //COMPILER_ARGS_H
